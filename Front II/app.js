de663fabe2b4c98784bc64b634f41cc2a6489a81
JeffersonMendes32 <99696674+JeffersonMendes32@users.noreply.github.com>
2022-05-09 23:54:47 -0300

    aula04 front 2

---
+++ Front II/app.js
@@ -0,0 +1,11 @@
+const nav = document.querySelector('#defaultHeader');
+nav.innerHTML = 
+`   <h1>O jornal de ontem</h1>
+  `;
+
+
+  
+  const changeColor = document.getElementById("defaultHeader")
+  changeColor.style.background = "green"
+
+ 
